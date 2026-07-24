8bb37ef28fd0ae123334a4cb57b3cce87a93f0c0
Terry Migwi <terrymigwi@gmail.com>
2026-07-24 06:54:31 +0300

    Increase topK to 10 for better retrieval

--- api/chat.js
+++ api/chat.js
@@ -41,7 +41,7 @@ export default async function handler(req, res) {
       },
       body: JSON.stringify({
         vector: vector,
-        topK: 5,
+        topK: 10,
         namespace: 'portfolio',
         includeMetadata: true
       })
